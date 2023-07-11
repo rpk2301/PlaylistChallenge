7ced1243badf8229abfc4dccd599a35fd159c187
ricky <rpk2301@vt.edu>
2023-07-11 17:59:24 -0400

    I have finished the lab.

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,86 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection)
+    {
+        int i =checkforwards(startIndex,selection);
+        int f = checkbackwards(startIndex,selection);
+
+        if(i<f)
+        {
+        return i;
+        }
+        else
+        {
+            return f;
+        }
+
     }
-}
+
+    public int checkforwards(int startIndex, String selection)
+    {
+        int i =startIndex;
+        int jumps =0;
+        boolean found = false;
+        while(found == false) {
+
+            if (playList[i].equals(selection))
+            {
+                found = true;
+            }
+            else if (i == playList.length - 1)
+            {
+                jumps++;
+                i = 0;
+            } else
+            {
+                jumps++;
+                i++;
+            }
+
+        }
+          return jumps;
+        }
+
+
+
+    public int checkbackwards(int startIndex, String selection)
+    {
+        int i =startIndex;
+        int jumps =0;
+        boolean found = false;
+        while(found == false) {
+
+            if (playList[i].equals(selection))
+            {
+                found = true;
+            }
+            else if (i == 0)
+            {
+                jumps++;
+                i = playList.length-1;
+            } else
+            {
+                jumps++;
+                i--;
+            }
+
+        }
+        return jumps;
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
